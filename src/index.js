4dc5ac44bfc0e3ebebdf1198830763f7d10d409f
Nikita213-hub <70839622+Nikita213-hub@users.noreply.github.com>
2021-07-07 18:33:21 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = array.reduce(function(a, b) {
+    console.log(Math.min(a, b));
+    return Math.min(a, b);
+  })
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = array.reduce(function(a, b) {
+    console.log(Math.max(a, b));
+    return Math.max(a, b);
+  })
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avg = 0;
+  avg = (array.reduce((sum, current) => sum + current, 0)) / array.length;
+  console.log(avg);
+  return avg;
 }
