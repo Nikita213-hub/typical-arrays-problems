a9f0f8d11ec1990a2e3ae8a3810bac7a4c996821
Nikita213-hub <70839622+Nikita213-hub@users.noreply.github.com>
2021-07-07 18:39:10 +0300

    refactor: improve code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 exports.min = function min (array) {
+  if(array.length <= 0){return 0;}
   let min = array.reduce(function(a, b) {
     console.log(Math.min(a, b));
     return Math.min(a, b);
@@ -7,6 +8,7 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
+  if(array.length <= 0){return 0;}  
   let max = array.reduce(function(a, b) {
     console.log(Math.max(a, b));
     return Math.max(a, b);
@@ -14,6 +16,7 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
+  if(array.length <= 0){return 0;}
   let avg = 0;
   avg = (array.reduce((sum, current) => sum + current, 0)) / array.length;
   console.log(avg);
